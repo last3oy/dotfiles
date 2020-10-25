c856f7d214654b8b0227afa46c16f068bcfc7ae1
last3oy <softwarez_gelion@hotmail.com>
2020-10-25 22:23:06 +0700

    updated

--- android/path.zsh
+++ android/path.zsh
@@ -1,2 +1,3 @@
 export ANDROID_HOME=/Users/$USER/Library/Android/sdk
-export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH"
+export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
+export JAVA_HOME="/Applications/Android Studio.app/Contents/jre/jdk/Contents/home/"
---
+++ ruby/path.zsh
@@ -0,0 +1,2 @@
+export PATH="/usr/local/opt/ruby/bin:$PATH"
+export PATH="/usr/local/opt/ruby@2.5/bin:$PATH"
